1417eaee785ef05f8d104b885aea6b0801c122b7
nathan-g1 <nathygech111@gmail.com>
2024-10-10 14:05:09 -0500

    Remove c99 standard
    - To avoid any issues on resulting assembly code

--- dot_product.c
+++ dot_product.c
@@ -1,7 +1,8 @@
 
 float dot_product(float *a, float *b, int length) {
     float result = 0;
-    for (int i = 0; i < length; i++) {
+    int i;
+    for (i = 0; i < length; i++) {
         result += a[i] * b[i];
     }
     return result;
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Compiling and running the program"
-gcc -m32 -march=corei7 -std=c99 -o a.out main.c dot_product.c
+gcc -m32 -march=corei7 -o a.out main.c dot_product.c
 time ./a.out
